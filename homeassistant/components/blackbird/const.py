30f931d3cdb0b58e202cff7c0b887aea56db636e
archiconda1976 <82009380+archiconda1976@users.noreply.github.com>
2024-08-18 16:16:01 -0500

    Update const.py to move other constants into const file

--- homeassistant/components/blackbird/const.py
+++ homeassistant/components/blackbird/const.py
@@ -2,3 +2,9 @@
 
 DOMAIN = "blackbird"
 SERVICE_SETALLZONES = "set_all_zones"
+CONF_ZONES = "zones"
+CONF_SOURCES = "sources"
+DEFUALT_PORT = 4001
+DATA_BLACKBIRD = "blackbird"
+DEFAULT_NAME = "blackbird matrix"
+ATTR_SOURCE = "source"
