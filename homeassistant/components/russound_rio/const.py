5920a0da86d298f51d750336da67b6981c02698c
Andy Archibald <ccetech@charter.net>
2024-10-01 22:40:52 -0500

    Favorite browsing support

--- homeassistant/components/russound_rio/const.py
+++ homeassistant/components/russound_rio/const.py
@@ -22,3 +22,22 @@ CONNECT_TIMEOUT = 5
 MP_FEATURES_BY_FLAG = {
     FeatureFlag.COMMANDS_ZONE_MUTE_OFF_ON: MediaPlayerEntityFeature.VOLUME_MUTE
 }
+
+MENU_SYSTEM_FAVORITE_TITLE = "System Favorites"
+MENU_SYSTEM_FAVORITE = "system"
+MENU_PLAY_SYSTEM_FAVORITE = "PlaySysFav"
+MENU_ZONE_FAVORITE_TITLE = "Zone Favorites"
+MENU_ZONE_FAVORITE = "zone"
+MENU_PLAY_ZONE_FAVORITE = "PlayZoneFav"
+MENU_SOURCE_TITLE = "Source Menu"
+MENU_SOURCE = "source_menu"
+RUSSOUND_STREAMER_SOURCE = "Russound Media Streamer"
+RUSSOUND_MEDIA_TITLE = "Russound Media Source Options"
+
+SERVICE_CALL_ATTR_FAVORITE_ID = "favorite_id"
+SERVICE_CALL_ATTR_ENTITY_ID = "entity_id"
+SERVICE_CALL_ATTR_FAVORITE_NAME = "name"
+SERVICE_SAVE_SYSTEM_FAVORITE = "save_system_favorite"
+SERVICE_SAVE_ZONE_FAVORITE = "save_zone_favorite"
+SERVICE_DELETE_SYSTEM_FAVORITE = "delete_system_favorite"
+SERVICE_DELETE_ZONE_FAVORITE = "delete_zone_favorite"
--- homeassistant/components/russound_rio/media_player.py
+++ homeassistant/components/russound_rio/media_player.py
@@ -2,13 +2,20 @@
 
 from __future__ import annotations
 
+from typing import Any
+import voluptuous as vol
+
 import logging
 
+from . import RussoundConfigEntry
 from aiorussound import Controller
-from aiorussound.models import Source
+from aiorussound.models import Source, Favorite
 from aiorussound.rio import ZoneControlSurface
 
 from homeassistant.components.media_player import (
+    BrowseError,
+    BrowseMedia,
+    MediaClass,
     MediaPlayerDeviceClass,
     MediaPlayerEntity,
     MediaPlayerEntityFeature,
@@ -16,14 +23,47 @@ from homeassistant.components.media_player import (
     MediaType,
 )
 from homeassistant.config_entries import SOURCE_IMPORT
-from homeassistant.core import DOMAIN as HOMEASSISTANT_DOMAIN, HomeAssistant
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+from homeassistant.core import (
+    DOMAIN as HOMEASSISTANT_DOMAIN,
+    Event,
+    HomeAssistant,
+    ServiceCall,
+)
+from homeassistant.helpers.entity import Entity
 from homeassistant.data_entry_flow import FlowResultType
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+import homeassistant.helpers.config_validation as cv
 
-from . import RussoundConfigEntry
-from .const import DOMAIN, MP_FEATURES_BY_FLAG
+from .const import (
+    DOMAIN,
+    MENU_PLAY_SYSTEM_FAVORITE,
+    MENU_PLAY_ZONE_FAVORITE,
+    MENU_SOURCE,
+    MENU_SOURCE_TITLE,
+    MENU_SYSTEM_FAVORITE,
+    MENU_SYSTEM_FAVORITE_TITLE,
+    MENU_ZONE_FAVORITE,
+    MENU_ZONE_FAVORITE_TITLE,
+    MP_FEATURES_BY_FLAG,
+    RUSSOUND_MEDIA_TITLE,
+    RUSSOUND_STREAMER_SOURCE,
+    SERVICE_CALL_ATTR_FAVORITE_ID,
+    SERVICE_SAVE_SYSTEM_FAVORITE,
+    SERVICE_SAVE_ZONE_FAVORITE,
+    SERVICE_DELETE_SYSTEM_FAVORITE,
+    SERVICE_DELETE_ZONE_FAVORITE,
+    SERVICE_CALL_ATTR_ENTITY_ID,
+    SERVICE_CALL_ATTR_FAVORITE_NAME,
+)
+
+MEDIA_PLAYER_SCHEMA = vol.Schema({SERVICE_CALL_ATTR_ENTITY_ID: cv.comp_entity_ids})
+RUSSOUND_SYSTEM_CALL_SCHEMA = MEDIA_PLAYER_SCHEMA.extend(
+    {vol.Required(SERVICE_CALL_ATTR_FAVORITE_ID): cv.string}
+)
+
 from .entity import RussoundBaseEntity, command
 
 _LOGGER = logging.getLogger(__name__)
@@ -92,6 +132,55 @@ async def async_setup_entry(
         for zone_id in controller.zones
     )
 
+    async def service_handle(service: ServiceCall) -> None:
+        """Handle for services."""
+        favorite_id = service.data.get(SERVICE_CALL_ATTR_FAVORITE_ID)
+        favorite_name = service.data.get(SERVICE_CALL_ATTR_FAVORITE_NAME)
+        entity_ids = service.data.get(SERVICE_CALL_ATTR_ENTITY_ID)
+
+        if favorite_id and entity_ids:
+            for entity in entities:
+                for entity_in in entity_ids:
+                    if entity.entity_id == entity_in:
+                        _LOGGER.debug("Found requested entity %s", entity_in)
+                        if service.service == SERVICE_SAVE_SYSTEM_FAVORITE:
+                            await entity.save_system_favorite(
+                                int(favorite_id), favorite_name
+                            )
+                        elif service.service == SERVICE_SAVE_ZONE_FAVORITE:
+                            await entity.save_zone_favorite(
+                                int(favorite_id), favorite_name
+                            )
+                        elif service.service == SERVICE_DELETE_SYSTEM_FAVORITE:
+                            await entity.delete_system_favorite(int(favorite_id))
+                        elif service.service == SERVICE_DELETE_ZONE_FAVORITE:
+                            await entity.delete_zone_favorite(int(favorite_id))
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_SAVE_SYSTEM_FAVORITE,
+        service_handle,
+        schema=RUSSOUND_SYSTEM_CALL_SCHEMA,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_SAVE_ZONE_FAVORITE,
+        service_handle,
+        schema=RUSSOUND_SYSTEM_CALL_SCHEMA,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_DELETE_SYSTEM_FAVORITE,
+        service_handle,
+        schema=RUSSOUND_SYSTEM_CALL_SCHEMA,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_DELETE_ZONE_FAVORITE,
+        service_handle,
+        schema=RUSSOUND_SYSTEM_CALL_SCHEMA,
+    )
+
 
 class RussoundZoneDevice(RussoundBaseEntity, MediaPlayerEntity):
     """Representation of a Russound Zone."""
@@ -104,6 +193,11 @@ class RussoundZoneDevice(RussoundBaseEntity, MediaPlayerEntity):
         | MediaPlayerEntityFeature.TURN_ON
         | MediaPlayerEntityFeature.TURN_OFF
         | MediaPlayerEntityFeature.SELECT_SOURCE
+        | MediaPlayerEntityFeature.VOLUME_MUTE
+        | MediaPlayerEntityFeature.BROWSE_MEDIA
+        | MediaPlayerEntityFeature.PREVIOUS_TRACK
+        | MediaPlayerEntityFeature.NEXT_TRACK
+        | MediaPlayerEntityFeature.PLAY_MEDIA
     )
 
     def __init__(
@@ -119,6 +213,56 @@ class RussoundZoneDevice(RussoundBaseEntity, MediaPlayerEntity):
         for flag, feature in MP_FEATURES_BY_FLAG.items():
             if flag in self._client.supported_features:
                 self._attr_supported_features |= feature
+
+    async def save_system_favorite(self, favorite_id: int, favorite_name=None) -> None:
+        """Save system favorite to controller."""
+
+        if favorite_name is None:
+            # default to channel name if no name is provided
+            favorite_name = self._current_source().properties.channel_name
+
+        if favorite_name is None:
+            # if no channel name, set a default name
+            favorite_name = f"F{favorite_id}"
+
+        if favorite_id >= 1 and favorite_id <= 32:
+            _LOGGER.debug("Saving system favorite %d", favorite_id)
+            await self._zone.send_event(
+                "saveSystemFavorite", f'"{favorite_name}"', favorite_id
+            )
+
+    async def save_zone_favorite(self, favorite_id: int, favorite_name=None) -> None:
+        """Save zone favorite to contoller."""
+
+        if favorite_name is None:
+            # default to channel name if no name is provided
+            favorite_name = self._current_source().properties.channel_name
+
+        if favorite_name is None:
+            # if no channel name, set a default name
+            favorite_name = f"F{favorite_id}"
+
+        if favorite_id >= 1 and favorite_id <= 32:
+            _LOGGER.debug("Saving zone favorite %d", favorite_id)
+            await self._zone.send_event(
+                "saveZoneFavorite", f'"{favorite_name}"', favorite_id
+            )
+
+    async def delete_system_favorite(self, favorite_id: int) -> None:
+        """Delete system favorite from contoller."""
+
+        if favorite_id >= 1 and favorite_id <= 32:
+            _LOGGER.debug("Removing system favorite %d", favorite_id)
+            await self._zone.send_event(
+                "KeyRelease", "deleteSystemFavorite", favorite_id
+            )
+
+    async def delete_zone_favorite(self, favorite_id: int) -> None:
+        """Delete zone favorite from contoller."""
+
+        if favorite_id >= 1 and favorite_id <= 2:
+            _LOGGER.debug("Removing system favorite %d", favorite_id)
+            await self._zone.send_event("KeyRelease", "deleteZoneFavorite", favorite_id)
 
     @property
     def _zone(self) -> ZoneControlSurface:
@@ -151,17 +295,32 @@ class RussoundZoneDevice(RussoundBaseEntity, MediaPlayerEntity):
     @property
     def media_title(self):
         """Title of current playing media."""
-        return self._source.song_name
+        if self._source.song_name != None:
+            return self._source.song_name
+        elif self._source.program_service_name != None:
+            return self._source.program_service_name
+        else:
+            return self._source.name
 
     @property
     def media_artist(self):
         """Artist of current playing media, music track only."""
-        return self._source.artist_name
+        if self._source.artist_name != None:
+            return self._source.artist_name
+        elif self._source.radio_text != None:
+            return self._source.radio_text
+        else:
+            return None
 
     @property
     def media_album_name(self):
         """Album name of current playing media, music track only."""
-        return self._source.album_name
+        if self._source.album_name != None:
+            return self._source.album_name
+        elif self._source.channel_name != None:
+            return self._source.channel_name
+        else:
+            return None
 
     @property
     def media_image_url(self):
@@ -211,3 +370,147 @@ class RussoundZoneDevice(RussoundBaseEntity, MediaPlayerEntity):
     async def async_volume_down(self) -> None:
         """Step the volume down."""
         await self._zone.volume_down()
+
+    async def async_browse_media(
+        self,
+        media_content_type: MediaType | str | None = None,
+        media_content_id: str | None = None,
+    ) -> BrowseMedia:
+        """Browse system favorites and zone favorites and MMMenus if supported by source."""
+
+        if not media_content_id:
+            return await self.async_browse_media_root()
+
+        path = media_content_id.partition("/")
+        if path[0] == MENU_SYSTEM_FAVORITE:
+            return await self.async_browse_media_system_favorites(True)
+        elif path[0] == MENU_ZONE_FAVORITE:
+            return await self.async_browse_media_zone_favorites(True)
+        elif MENU_PLAY_SYSTEM_FAVORITE in path[0]:
+            await self._zone.send_event("restoreSystemFavorite", media_content_id[11:])
+            return
+        elif MENU_PLAY_ZONE_FAVORITE in path[0]:
+            await self._zone.send_event("restoreZoneFavorite", media_content_id[13:])
+            return
+
+        raise BrowseError(f"Media not found: {media_content_type} / {media_content_id}")
+
+    async def async_browse_media_root(self) -> BrowseMedia:
+        """Return root media objects."""
+
+        return BrowseMedia(
+            title=RUSSOUND_MEDIA_TITLE,
+            media_class=MediaClass.DIRECTORY,
+            media_content_id="",
+            media_content_type="",
+            can_play=False,
+            can_expand=True,
+            children=[
+                await self.async_browse_media_zone_favorites(),
+                await self.async_browse_media_system_favorites(),
+            ],
+        )
+
+    def _get_source_from_id(self, source_id_in) -> str:
+        for source_id in self._sources:
+            if source_id == int(source_id_in):
+                return str(self._sources[source_id].name)
+        return "unknown"
+
+    def _get_menu_title(self, favorite: Favorite) -> str:
+        return (
+            "["
+            + self._get_source_from_id(favorite.source_id)
+            + "] \r\n"
+            + favorite.name
+        )
+
+    async def async_browse_media_zone_favorites(
+        self, expanded: bool = False
+    ) -> BrowseMedia:
+        """Return zone favorite objects."""
+
+        if expanded:
+            zone_favorites = await self._zone.enumerate_favorites()
+            children = [
+                BrowseMedia(
+                    title=self._get_menu_title(fav),
+                    media_class=MediaClass.DIRECTORY,
+                    media_content_id=f"{MENU_PLAY_ZONE_FAVORITE}:{fav.favorite_id}",
+                    media_content_type=MediaType.MUSIC,
+                    can_play=True,
+                    can_expand=False,
+                    thumbnail=fav.album_cover_url,
+                )
+                # for uri, item in self.coordinator.source_map.items()
+                for fav in zone_favorites
+                if fav.favorite_id > 0
+            ]
+        else:
+            children = None
+
+        return BrowseMedia(
+            title=MENU_ZONE_FAVORITE_TITLE,
+            media_class=MediaClass.DIRECTORY,
+            media_content_id=MENU_ZONE_FAVORITE,
+            media_content_type=MediaType.MUSIC,
+            children_media_class=MediaClass.MUSIC,
+            can_play=False,
+            can_expand=True,
+            children=children,
+        )
+
+    async def async_browse_media_system_favorites(
+        self, expanded: bool = False
+    ) -> BrowseMedia:
+        """Return system favorite objects."""
+
+        if expanded:
+            system_favorites = await self._zone.client.enumerate_system_favorites()
+            children = [
+                BrowseMedia(
+                    title=self._get_menu_title(favorite),
+                    media_class=MediaClass.DIRECTORY,
+                    media_content_id=f"{MENU_PLAY_SYSTEM_FAVORITE}:{favorite.favorite_id}",
+                    media_content_type=MediaType.MUSIC,
+                    can_play=True,
+                    can_expand=False,
+                    thumbnail=favorite.album_cover_url,
+                )
+                for favorite in system_favorites
+                if favorite.favorite_id > 0
+            ]
+        else:
+            children = None
+
+        return BrowseMedia(
+            title=MENU_SYSTEM_FAVORITE_TITLE,
+            media_class=MediaClass.DIRECTORY,
+            media_content_id=MENU_SYSTEM_FAVORITE,
+            media_content_type=MediaType.MUSIC,
+            children_media_class=MediaClass.MUSIC,
+            can_play=False,
+            can_expand=True,
+            children=children,
+        )
+
+    async def async_play_media(
+        self, media_type: MediaType | str, media_id: str, **kwargs: Any
+    ) -> None:
+        if media_type == MediaType.MUSIC:
+            if MENU_PLAY_SYSTEM_FAVORITE in media_id:
+                await self._zone.send_event("restoreSystemFavorite", media_id[11:])
+            elif MENU_PLAY_ZONE_FAVORITE in media_id:
+                await self._zone.send_event("restoreZoneFavorite", media_id[12:])
+            else:
+                _LOGGER.error(f"****** media_id: '{media_id}'")
+        else:
+            _LOGGER.error(f"****** media_type: '{media_type}'")
+
+    async def async_media_next_track(self):
+        """Next Track."""
+        await self._zone.send_event("KeyRelease", "Next")
+
+    async def async_media_previous_track(self):
+        """Previous Track."""
+        await self._zone.send_event("KeyRelease", "Previous")
